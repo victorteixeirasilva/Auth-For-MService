952ed48a24c8d65e9becb31881e18a09847c5f02
victorteixeriasilva <victorteixeira2308@gmail.com>
2025-11-08 18:34:25 -0300

    ÃAjustando Bean

--- AuthForMService/src/main/java/tech/inovasoft/inevolving/ms/AuthForMService/config/KeyConfig.java
+++ AuthForMService/src/main/java/tech/inovasoft/inevolving/ms/AuthForMService/config/KeyConfig.java
@@ -3,6 +3,7 @@ package tech.inovasoft.inevolving.ms.AuthForMService.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.KeyFactory;
@@ -10,14 +11,17 @@ import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import org.springframework.core.io.ClassPathResource;
 import java.util.Base64;
 
 @Configuration
 public class KeyConfig {
 
+
     @Bean
     public RSAPrivateKey rsaPrivateKey() throws Exception {
-        String key = Files.readString(Path.of("src/main/resources/private.pem"));
+        ClassPathResource resource = new ClassPathResource("private.pem");
+        String key = new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
         key = key.replace("-----BEGIN PRIVATE KEY-----", "")
                 .replace("-----END PRIVATE KEY-----", "")
                 .replaceAll("\\s", "");
@@ -29,7 +33,8 @@ public class KeyConfig {
 
     @Bean
     public RSAPublicKey rsaPublicKey() throws Exception {
-        String key = Files.readString(Path.of("src/main/resources/public.pem"));
+        ClassPathResource resource = new ClassPathResource("public.pem");
+        String key = new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
         key = key.replace("-----BEGIN PUBLIC KEY-----", "")
                 .replace("-----END PUBLIC KEY-----", "")
                 .replaceAll("\\s", "");
